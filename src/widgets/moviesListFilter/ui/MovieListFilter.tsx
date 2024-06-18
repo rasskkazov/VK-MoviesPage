ec658b9a76787062d7d6325e814454f924a31190
Bogdan <brasskazov@gmail.com>
2024-06-18 18:42:10 +0300

    fix: apply filters desk bug

--- src/widgets/moviesListFilter/ui/MovieListFilter.tsx
+++ src/widgets/moviesListFilter/ui/MovieListFilter.tsx
@@ -10,7 +10,7 @@ export const MovieListFilter = (options: { modalBack?: VoidFunction }) => {
     <form
       onSubmit={(e) => {
         submit(e);
-        options.modalBack();
+        if (options.modalBack) options.modalBack();
       }}
     >
       <GenreFilter dispatch={dispatch} />
